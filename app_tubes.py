1aaff05fd4aa50422d8d7cb2fc3d40ec2bcf7842
Kesyasakha <90273773+KesyaSakha@users.noreply.github.com>
2024-06-19 22:35:28 +0700

    Update app_tubes.py

--- app_tubes.py
+++ app_tubes.py
@@ -192,16 +192,14 @@ def main():
         plt.title("Top 40 Comparison Chart - Bar Chart")
         st.pyplot(fig)
 
-         st.write("2. RELATIONSHIP CHART - SCATTER PLOT")
-        df_sel2 = df[['Runtime (mins)', 'IMDb Rating']].sort_values(by=['Runtime (mins)'], ascending=False).head(10))
+        st.write("2. RELATIONSHIP CHART - SCATTER PLOT")
+        df_sel2 = df[['Runtime (mins)', 'IMDb Rating']].sort_values(by=['Runtime (mins)'])
         st.write("### Data Table")
         st.dataframe(df_sel2)
         fig, ax = plt.subplots(figsize=(10, 6))
         sns.scatterplot(data=df_sel2, x='Runtime (mins)', y='IMDb Rating', hue='IMDb Rating', palette=pastel_colors, s=100, ax=ax)
-        ax.legend(bbox_to_anchor=(1, 1), loc='upper left')  # Adjust legend position
         plt.title("Relationship Chart - Scatter Plot")
         st.pyplot(fig)
-
 
         st.write("3. COMPOSITION CHART - DONUT CHART (Top 10 Genres)")
         genres = df['Genres'].str.split(',').explode().str.strip()

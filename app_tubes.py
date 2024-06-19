1eb6fc736014f783eff80ca04c9945d6d2a393e2
Kesyasakha <90273773+KesyaSakha@users.noreply.github.com>
2024-06-19 22:00:45 +0700

    Update app_tubes.py

--- app_tubes.py
+++ app_tubes.py
@@ -193,7 +193,7 @@ def main():
         st.pyplot(fig)
 
         st.write("2. RELATIONSHIP CHART - SCATTER PLOT")
-        df_sel2 = df.sort_values(by=['IMDb Rating'], ascending=False).head(40)[['Runtime (mins)', 'IMDb Rating']].sort_values(by=['Runtime (mins)'])
+        df_sel2 = df.sort_values(by=['IMDb Rating'], ascending=False).head(30)[['Runtime (mins)', 'IMDb Rating']].sort_values(by=['Runtime (mins)'])
         st.write("### Data Table")
         st.dataframe(df_sel2)
         fig, ax = plt.subplots(figsize=(10, 6))
